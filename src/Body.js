f340a86f8759833d78830767b87e0df200223456
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-04-03 19:38:49 +0530

    pagination implemented

--- src/Body.js
+++ src/Body.js
@@ -1,18 +1,62 @@
 import { useState, useEffect } from "react";
 import Table from './components/Table';
 import useFetch from './useFetch';
+import ReactPaginate from "react-paginate";
 
 const Body = () => {
-      const { error, isPending, data } = useFetch(
-        "https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI"
-    );
-    // const [start, setStart] = useState(0);
+//   const [pageCount, setpageCount] = useState(10);
+  const perPage = 10;
+  const [currentPage, setCurrentPage] = useState(0);
+  const { error, isPending, data } = useFetch(
+    "https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI"
+  );
+
+  const handlePageClick = ({ selected: selectPage }) => {
+    console.log("selected page", selectPage);
+    setCurrentPage(selectPage);
+  };
+
+  const offset = currentPage * perPage;
+  console.log(offset);
+  // console.log(data)
+    let pageCount = 10;
+    let currentData=[];
+    if (!isPending) {
+   pageCount = Math.ceil(data.length / perPage);
+
+ currentData = data.slice(offset, offset + perPage);
+
+    }
+
+  
     
-    return ( 
-        <>
-           { !isPending &&<Table bank_data={data } />}
-        </>
-     );
-}
+
+  return (
+    <>
+      {!isPending && <Table bank_data={currentData} />}
+      {!isPending && (
+        <ReactPaginate
+          previousLabel={"previous"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination justify-content-center"}
+          pageClassName={"page-item"}
+          pageLinkClassName={"page-link"}
+          previousClassName={"page-item"}
+          previousLinkClassName={"page-link"}
+          nextClassName={"page-item"}
+          nextLinkClassName={"page-link"}
+          breakClassName={"page-item"}
+          breakLinkClassName={"page-link"}
+          activeClassName={"active"}
+        />
+      )}
+    </>
+  );
+};
 
 export default Body;
