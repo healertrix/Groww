deb0decb0a2fcf48f6c71d8f833a1666dd88a524
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-04-04 22:34:39 +0530

    404 page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Home from "./Home";
 import BankCard from "./components/BankCard"
+import ErrorPage from './components/404'
 import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/all-banks" element={<Home />} />
         <Route path="/bank-details/:id" element={<BankCard />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
--- src/components/CardOfBank.js
+++ src/components/CardOfBank.js
@@ -4,7 +4,9 @@ const CardOfBank = ({ bank_name, ifsc, branch, address,city,district,state }) =>
       <>
         <>
           <div className="w-0.85 h-full card	bg-base-100 shadow-xl  flex gap-3 m-2 p-2">
-            <div className="text-center text-3xl underline mb-5	">Bank Details</div>
+            <div className="text-center text-3xl underline mb-5	">
+              Bank Details
+            </div>
             <div className="flex gap-2 m-2">
               <p className=" decoration-2 font-bold	">BANK NAME : </p>
               <p>{bank_name} </p>
@@ -33,7 +35,7 @@ const CardOfBank = ({ bank_name, ifsc, branch, address,city,district,state }) =>
               <p className=" decoration-2 font-bold	">Address : </p>
               <p>{address}</p>
             </div>
-            <Link to="/">
+            <Link to="/all-banks">
               <div className="card-actions flex gap-2 m-2">
                 <button className="btn btn-primary">Back To Home</button>
               </div>
